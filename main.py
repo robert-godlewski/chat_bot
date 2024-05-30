11c6f626394e5d767fe3792cdf72db945cf533ad
Robert Godlewski <robtechg5@gmail.com>
2024-05-30 15:48:16 -0700

    jsonify the responses

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-responses = {
-    'exit': 'Ok, good bye',
-    'default': 'Sorry, please elaborate...',
-    'greeting': 'How may I help you today? '
-}
+import json
+
 
 exit_keys = ('exit','quit','q')
 
 
 if __name__ == '__main__':
     print("Hello there, I am a chat bot built by Robert Godlewski.")
-    info = input(responses['greeting'])
-    while True:
+    try:
+        data = open('responses.json').read()
+        responses = json.loads(data)
+        print(responses['greeting'])
+    except:
+        print("responses are not properly loaded")
+        responses = None
+    info = None
+    while responses:
+        info = input("> ")
+        print("Respond with q to exit.")
         if info in exit_keys:
             print(responses['exit'])
             break
+        elif info == 'hello' or info == '2':
+            print(responses['greeting'])
         elif info in responses:
             print(responses[info])
         else:
-            print(responses['default'])
-        info = input("Respond with q to exit or type query here: ")
+            i = 0
+            while i < len(responses['confused']):
+                print(responses['confused'][i])
+                i += 1
